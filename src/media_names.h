3946f28f2a69b1714b9865f131bbd1f153375769
nudon <hessryan96@gmail.com>
2019-07-28 13:08:21 -0700

    forgot a file

---
+++ src/media_names.h
@@ -0,0 +1,10 @@
+#ifndef FILE_MEDIA_NAMES_SEEN
+#define FILE_MEDIA_NAMES_SEEN
+
+#define MEDIA_FOLDER "./media/"
+#define CRAB_FN MEDIA_FOLDER"crab.png"
+#define SAND_FN MEDIA_FOLDER"sand.png"
+#define EYE_FN  MEDIA_FOLDER"eye.png"
+
+
+#endif
