af9e3753d1378295029cedf04d1b60b110551cee
nudon <hessryan96@gmail.com>
2019-11-24 08:14:34 -0800

    added support for basic grabbing/throwing of things, reworked some internals

---
+++ src/names.h
@@ -0,0 +1,12 @@
+#ifndef FILE_NAMES_SEEN
+#define FILE_NAMES_SEEN
+
+#define BACKGROUND_PLANE_NAME "background"
+#define FOREGROUND_PLANE_NAME "foreground"
+#define MAIN_PLANE_NAME "main"
+
+#define TRASHCAN_SPAWN "trashcan_spawn"
+#define BLUE_SLIME_SPAWN "blue_slime_spawn"
+#define TEST_SPAWN "test_spawn"
+#define GOHEI_SPAWN "gohei_spawn"
+#endif
